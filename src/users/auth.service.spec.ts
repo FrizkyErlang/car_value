437b8b8ede146793bc4a314c8a634857f5e1fe34
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-03 22:34:01 +0700

    finish unit testing scenario

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common/exceptions';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
@@ -5,13 +6,25 @@ import { User } from './users.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of users service
-    const fakeUsersService: Partial<UsersService> = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -32,11 +45,40 @@ describe('AuthService', () => {
   });
 
   it('create a new user with a salted and hashed password', async () => {
-    const user = await service.signup('testEmail@test.com', 'testPass123');
+    const user = await service.signup('testemail@test.com', 'testpass123');
 
-    expect(user.password).not.toEqual('testPass123');
+    expect(user.password).not.toEqual('testpass123');
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user sign up with email that is in use', async () => {
+    await service.signup('usedemail@test.com', 'testpass123first');
+
+    await expect(
+      service.signup('usedemail@test.com', 'testpass123second'),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('throws an error if sign in with unused email', async () => {
+    await expect(
+      service.signin('unusedemail@test.com', 'testpass123'),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('throws an error if user sign in with invalid password', async () => {
+    await service.signup('invalidpass@test.com', 'testpass123true');
+
+    await expect(
+      service.signin('invalidpass@test.com', 'testpass123false'),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('return a user if sign in with correct password', async () => {
+    await service.signup('validpass@test.com', 'testpass123');
+
+    const user = await service.signin('validpass@test.com', 'testpass123');
+    expect(user).toBeDefined();
   });
 });
