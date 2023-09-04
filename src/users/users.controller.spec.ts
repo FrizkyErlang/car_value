cfe4b5c6b78b3119ec96f8eb02a4785d1cfaa39a
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-04 11:11:18 +0700

    make userController unit test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -30,7 +30,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -42,7 +44,7 @@ describe('UsersController', () => {
         },
         {
           provide: AuthService,
-          useValue: fakeUsersService,
+          useValue: fakeAuthService,
         },
       ],
     }).compile();
@@ -71,5 +73,16 @@ describe('UsersController', () => {
     fakeUsersService.findOne = () => null;
 
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin update session object and returns user', async () => {
+    const session = { userId: null };
+    const user = await controller.signin(
+      { email: 'test@test.com', password: 'testpass' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
