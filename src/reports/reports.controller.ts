f8b14d92cf588d90c9435e59bab6dd90382d3e02
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-07 14:49:51 +0700

    serialize reportDto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
 import { User } from '../users/users.entity';
 
 @Controller('reports')
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(body, user);
   }
