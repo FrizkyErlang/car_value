13e7fe2539504ff46097e55e64f7b6335e3d30c4
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-07 15:14:13 +0700

    implement approval report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Body,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { User } from '../users/users.entity';
 
 @Controller('reports')
@@ -16,5 +24,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(id, body.approved);
   }
 }
