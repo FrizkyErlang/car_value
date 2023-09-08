3ff100d60f59d5a2a2f48443ea29657e999a92ec
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-08 09:42:42 +0700

    fix file naming

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './users.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user..interceptors';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptors';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
