eccfa6dddb4ec8a798429c2e86d16ca06c5c1a98
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-09-08 10:10:19 +0700

    implement admin guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { AdminGuard } from '../guards/admin.guard';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -27,6 +28,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportService.changeApproval(id, body.approved);
   }
